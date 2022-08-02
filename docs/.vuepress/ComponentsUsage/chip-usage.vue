6a8561654f8aa02d91c96418fc7cbfa044ed5fd8
Umut KENAR <umut.kenar@hotmail.com>
2022-08-02 17:24:47 +0300

    feat: chip usage init

--- docs/.vuepress/ComponentsUsage/chip-usage.vue
+++ docs/.vuepress/ComponentsUsage/chip-usage.vue
@@ -2,17 +2,47 @@
   <usage>
     <template #title>Chip</template>
     <template #component>
-      
+      <y-chip :close="close">Sample Chip</y-chip>
+    </template>
+    <template #bottomProps>
+      <y-checkbox
+          @change="setClose"
+          inputValue="close"
+          label="Close"
+          class="gap-2"
+        ></y-checkbox>
     </template>
   </usage>
   <copy-to-clip-board>
-    &lt;y-collapsible-panel title="Collapsible Panel"{{
-      "\n"
-    }}&nbsp;&gt;&lt;/y-collapsible-panel&gt;
+    &lt;y-chip{{close ? ' close @close=""':''}}&gt;
+      {{'\n  '}}Sample Chip{{'\n '}}&lt;/y-chip&gt;
   </copy-to-clip-board>
+  <div class="flex flex-col mt-7 gap-1">
+    <p class="font-semibold text-xl">Component Props</p>
+    <table>
+      <thead>
+        <tr>
+          <th>Prop Name</th>
+          <th>Type</th>
+          <th>Default</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>close</td>
+          <td>Boolean</td>
+          <td>false</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+const close= ref(false);
 
+function setClose() {
+  close.value = !close.value;
+}
 </script>
